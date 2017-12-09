55a72735f9996355d70ddfafe4fcb607f207e4f8
Coen Munckhof <c.munckhof@gmail.com>
2017-12-09 22:42:40 +0100

    Backupscript using rsync and ssh (if remote destination)

---
+++ backup/backup.sh
@@ -0,0 +1,316 @@
+#!/bin/sh
+
+
+#***************************************************************
+VERSION="2017.12.07"
+
+DATETIME_START="$(date +"%Y.%m.%d-%H.%M.%S")"
+
+
+# Default backup excludes and default options.
+RSYNC_EXCLUDE_DSM='--exclude=#recycle/ --exclude=@eaDir/'
+RSYNC_EXCLUDE_MAC='--exclude=.Trashes/ --exclude=.TemporaryItems/'
+RSYNC_DEFAULT_OPTIONS='--hard-links --delete --delete-excluded --archive --chmod=oga-w'
+
+
+# Get absolute path this script is in and use this path as a base for all other (relatve) filenames.
+# !! Please make sure there are no spaces inside the path !!
+# Source: https://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+# 2017-12-07
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+CONFIG_DIR=${SCRIPTPATH}/configs
+
+echo $SCRIPTPATH
+echo $CONFIG_DIR
+
+#file containing global information 
+GLOBAL_CONFIG=global.config
+
+
+#***************************************************************
+# Functions  
+#***************************************************************
+
+print_header()
+{
+	echo ""
+	echo ""
+	echo "#################################################"
+	echo "#   $0 - version $VERSION" 
+	echo "#################################################"
+	echo ""
+}
+
+
+print_help()
+{
+	echo "Usage:                                                                   "
+	echo "                                                                         "
+	echo "  $0 -s <source config> -d <destination config> [-c -p -v -q -h]         "
+	echo "                                                                         "
+	echo "	-h = Help                                                              "
+	echo "       List this help menu                                               "
+	echo "                                                                         "
+	echo "	-s = Source of rsync backup                                            "
+	echo "                                                                         " 
+	echo "	-d = Destination type of the backup (remote or local)                  "
+	echo "                                                                         "
+	echo "	-c = Checksum mode                                                     "
+	echo "       Forces sender to checksum all files before transfer.              "
+	echo "       Can be quite slow.                                                "
+    echo "                                                                         "	
+	echo "	-v = Verbose mode                                                      "
+	echo "       Run rsync command with the -v switch to use verbose output        " 
+    echo "																		   "	
+	echo "	-p = Progress mode                                                     "
+	echo "       Run rsync command with the -p switch to show the progress         " 
+	echo "																		   "
+	echo "	-q = Quiet mode                                                        "
+	echo "       Run rsync command with -q switch to suppress all output           "
+	echo "       except errors                                                     "
+	echo "																		   "
+	echo "----------------------------------------------------------------------------"
+}
+
+
+if [ ! -f ${SCRIPTPATH}/${GLOBAL_CONFIG} ]; then
+	print_header
+	echo [ERROR] File ${SCRIPTPATH}/${GLOBAL_CONFIG} does not exist.
+	echo
+	print_help
+	exit 1
+fi
+
+# At this moment, I know I can trust global.config
+# TODO: security check
+source ${SCRIPTPATH}/${GLOBAL_CONFIG}
+
+# assume this is an absolute dir.
+if [ -f ${CONFIG_EXCLUDE_FILE} ]; then
+	CONFIG_EXCLUDE_FILE='--exclude-from '${CONFIG_EXCLUDE_FILE}
+fi	
+
+#Wrapping up the excludes
+RSYNC_EXCLUDES="$RSYNC_EXCLUDE_DSM $RSYNC_EXCLUDE_MAC $CONFIG_EXCLUDE_FILE"
+
+
+
+#***************************************************************
+# Get Options from the command line  
+#***************************************************************
+while getopts "s:d:hcvpq" options
+do
+	case $options in 
+		c ) RSYNC_MODE_CHECKSUM='--checksum ';;
+		v ) RSYNC_MODE_VERBOSE='--verbose ';;
+		q ) RSYNC_MODE_QUIET='--quiet ';;
+		p ) RSYNC_MODE_PROGRESS='--progress ';;		
+		
+		s ) opt_s=$OPTARG;;
+		d ) opt_d=$OPTARG;;
+		e ) opt_e=$OPTARG;;
+		l ) opt_l=$OPTARG;;
+
+		h ) opt_h=1;;
+	esac
+done
+
+
+ 
+#***************************************************************
+# Print Help 
+#***************************************************************
+if [ $opt_h ]; then
+	print_header
+	print_help
+	exit 1
+fi 
+
+
+#***************************************************************
+# Destination 
+#***************************************************************
+if [ $opt_d ]; then
+
+	DESTINATION_CONFIG_FILE=backup.${opt_d}.config
+	if [ -f ${CONFIG_DIR}/${DESTINATION_CONFIG_FILE} ]; then
+		source ${CONFIG_DIR}/${DESTINATION_CONFIG_FILE}
+		# TODO: check if the DESTINATION_CONFIG_FILE file contains the required variables and check their values.
+	else
+		print_header
+		echo [ERROR] Destination config ${DESTINATION_CONFIG_FILE} does not exist.
+		exit 1
+	fi
+else
+	print_header
+	echo [ERROR] Destination config -d is empty
+	echo
+	print_help
+	exit 1	
+fi
+	
+BACKUP_DIR=$DEST_BACKUP_DIR
+
+
+ 
+#***************************************************************
+# Source
+#***************************************************************
+if [ $opt_s ]; then
+
+	BACKUPSET_CONFIG_FILE=backupset.${opt_s}.config
+	if [ -f ${CONFIG_DIR}/${BACKUPSET_CONFIG_FILE} ]; then
+		source ${CONFIG_DIR}/${BACKUPSET_CONFIG_FILE}
+		# TODO: check if the BACKUPSET_CONFIG_FILE file contains the required variables and check their values.
+	else
+		print_header
+		echo [ERROR] Backupset config ${BACKUPSET_CONFIG_FILE} does not exist
+		exit 1		
+	fi	
+
+	## Create the destination path (also the escaped variant)
+	DESTINATION_DIR=${DEST_BACKUP_DIR}/${BACKUP_NAME}
+	DESTINATION_DIR_ESCAPED=${DESTINATION_DIR// /\\ }
+
+else
+
+	print_header
+	echo [ERROR] Backupset config -s is empty
+	echo
+	print_help	
+	exit 1
+
+fi
+
+
+#***************************************************************
+# Misc
+#***************************************************************
+
+# Construct logfiles
+LOG_FILE=${DATETIME_START}_${opt_d}_${opt_s}.log
+LOG_FILE_BUSY=${CONFIG_LOG_DIR_BUSY}/${LOG_FILE}
+LOG_FILE_FINISHED=${CONFIG_LOG_DIR_FINISHED}/${LOG_FILE}
+LOG_FILE=
+
+
+#Check if source exists
+if [ ! -d "$BACKUP_SOURCE_DIR" ]; then
+	print_header
+	echo [ERROR] $BACKUP_SOURCE_DIR does not exist.
+	exit 1
+fi
+
+
+#***************************************************************
+# Run the real backup
+#***************************************************************
+echo Started at $DATETIME_START using backupscript $VERSION >> ${LOG_FILE_BUSY}
+
+
+if [ $DEST_IS_REMOTE -eq 1 ]; then
+
+	if [ $DEST_PORT -ne 22 ]; then
+		SSH_PORT='-p '$DEST_PORT
+	fi
+	
+		
+	# Check if private key file exists. If not -> quit backup.
+	if [ ! -f ${DEST_KEYFILE} ]; then
+		echo [ERROR] Cannot backup to remote because the private key ${DEST_KEYFILE} does not exist.
+		echo
+		print_help
+		exit 1
+	fi
+	SSH_KEY='-i '${DEST_KEYFILE}		
+		
+			
+	echo Start remote backup >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+
+	echo Create working dir for backup >> ${LOG_FILE_BUSY}
+	
+	ssh \
+		$SSH_PORT $SSH_KEY ${DEST_USER}@${DEST_HOST} \
+		"mkdir -p \"$DESTINATION_DIR/incomplete/\" && mkdir -p \"$DESTINATION_DIR/partial/\""
+		
+	echo >> ${LOG_FILE_BUSY}
+	
+	echo Start RSync >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+	echo ${DESTINATION_DIR_ESCAPED}  >> ${LOG_FILE_BUSY}
+	
+	rsync \
+		$RSYNC_MODE_CHECKSUM \
+		$RSYNC_MODE_VERBOSE \
+		$RSYNC_MODE_PROGRESS \
+		$RSYNC_MODE_QUIET \
+		${RSYNC_DEFAULT_OPTIONS} \
+		${RSYNC_EXCLUDES} \
+		-e 'ssh -p '${DEST_PORT}' -i'${DEST_KEYFILE} \
+		--link-dest="${DESTINATION_DIR_ESCAPED}/current"  \
+		"${BACKUP_SOURCE_DIR}" \
+		${DEST_USER}@${DEST_HOST}:"${DESTINATION_DIR_ESCAPED}/incomplete/" >> ${LOG_FILE_BUSY} 2>&1
+		
+		
+	echo >> ${LOG_FILE_BUSY}
+	
+	echo wrapping up... >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+	
+	ssh \
+		$SSH_PORT $SSH_KEY ${DEST_USER}@${DEST_HOST} \
+		"cd \"$DESTINATION_DIR\" &&  mv incomplete/ $DATETIME_START/ && rm -rf current && ln -s $DATETIME_START current && rm -rf partial"
+	
+else
+
+	echo start local backup >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+	
+	echo Create working dir for backup >> ${LOG_FILE_BUSY}
+	
+	mkdir -p "${DESTINATION_DIR}/current"
+	mkdir -p "${DESTINATION_DIR}/incomplete/"
+	mkdir -p "${DESTINATION_DIR}/partial/"
+
+	echo
+
+	echo Start RSync >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+	
+	rsync \
+		$RSYNC_MODE_CHECKSUM \
+		$RSYNC_MODE_VERBOSE \
+		$RSYNC_MODE_PROGRESS \
+		$RSYNC_MODE_QUIET \
+		${RSYNC_DEFAULT_OPTIONS} \
+		${RSYNC_EXCLUDES} \
+		--link-dest="${DESTINATION_DIR}/current" \
+		"${BACKUP_SOURCE_DIR}" \
+		"${DESTINATION_DIR}/incomplete/" >> ${LOG_FILE_BUSY} 2>&1
+
+		
+	echo >> ${LOG_FILE_BUSY}
+	
+	echo wrapping up... >> ${LOG_FILE_BUSY}
+	echo >> ${LOG_FILE_BUSY}
+	cd "${DESTINATION_DIR}"
+
+	mv incomplete/ ${DATETIME_START}/ 
+	rm -rf current 
+	ln -s $DATETIME_START current
+	rm -rf partial	
+fi
+
+
+# .. and we are done...
+DATETIME_FINISHED="$(date +"%Y.%m.%d-%H.%M.%S")"
+
+echo Finished at $DATETIME_FINISHED >> ${LOG_FILE_BUSY}
+echo Backup finished
+
+mv ${LOG_FILE_BUSY} ${LOG_FILE_FINISHED}
+
+exit 
