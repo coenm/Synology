12e1e94c3859ab73defb5e409cf27db8ef731af5
Coen Munckhof <c.munckhof@gmail.com>
2019-11-08 20:27:54 +0100

    entrypoint shebang bin/bash. copy ssh_host files to /etc/ssh/

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+## #!/bin/sh
 
 set -e
 
-if [[ -z ${SSH_USERNAME} ]]; then
+if [ -z ${SSH_USERNAME} ]; then
     echo "SSH_USERNAME environment variable not set."
-    exit -100  
+    exit 100
 fi
 
-if [[ ! -f "/home/${SSH_USERNAME}/" ]]; then
-    echo "home directory does't exist."
-    exit -101  
+
+if [[ ! -d "/home/${SSH_USERNAME}/" ]]; then
+    echo home directory /home/${SSH_USERNAME}/ does not exist!
+    exit 101
 fi
 
 if [[ ! -f "/tmp/.ssh/authorized_keys" ]]; then
     echo "no authorized keys file found."
-    exit -102  
+    exit 102
 fi
 
 if [[ -z ${BACKUP_DESTINATION} ]]; then
     echo "BACKUP_DESTINATION environment variable not set."
-    exit -103
+    exit 200
 fi
 
 
@@ -32,6 +35,33 @@ chmod 700 /home/${SSH_USERNAME}/.ssh
 chmod 600 /home/${SSH_USERNAME}/.ssh/authorized_keys
 # chmod 644 /home/${SSH_USERNAME}/.ssh/id_rsa.pub
 # chmod 600 /home/${SSH_USERNAME}/.ssh/id_rsa
+
+
+# Copy HostKeys when they are available. Otherwise, use existing ones.
+if [[ -f "/tmp/.ssh/ssh_host_ed25519_key" && -f "/tmp/.ssh/ssh_host_ed25519_key.pub" ]]; then
+    echo ssh_host_ed25519_key exists
+    rm /etc/ssh/ssh_host_ed25519_key
+    rm /etc/ssh/ssh_host_ed25519_key.pub
+    cp /tmp/.ssh/ssh_host_ed25519_key /etc/ssh/
+    cp /tmp/.ssh/ssh_host_ed25519_key.pub /etc/ssh/
+
+    chmod 644 /etc/ssh/ssh_host_ed25519_key.pub
+    chmod 600 /etc/ssh/ssh_host_ed25519_key
+    echo done ed25519
+fi
+
+if [[ -f "/tmp/.ssh/ssh_host_rsa_key" && -f "/tmp/.ssh/ssh_host_rsa_key.pub" ]]; then
+    echo ssh_host_rsa_key exists
+    rm /etc/ssh/ssh_host_rsa_key
+    rm /etc/ssh/ssh_host_rsa_key.pub
+    cp /tmp/.ssh/ssh_host_rsa_key /etc/ssh/
+    cp /tmp/.ssh/ssh_host_rsa_key.pub /etc/ssh/
+
+    chmod 644 /etc/ssh/ssh_host_rsa_key.pub
+    chmod 600 /etc/ssh/ssh_host_rsa_key
+    echo done ssh_host_rsa_key
+fi
+
 
 # set permissions for backup dir
 # TODOOOO: also find out what permissions
